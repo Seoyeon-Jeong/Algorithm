8c383d19d5ee4bf278db31cda9c0e216fe731f74
정서연 <116091047+Seoyeon-Jeong@users.noreply.github.com>
2025-04-08 15:19:02 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.09 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int i = 0; i < queries.length; i++ ) {
+            int x = arr[queries[i][0]];
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]] = x;
+        }
+        return arr;
+    }
+}
