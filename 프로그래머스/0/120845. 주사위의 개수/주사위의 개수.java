01a687337b4db00b6dcb3842e9424734de1fabc5
정서연 <116091047+Seoyeon-Jeong@users.noreply.github.com>
2025-05-21 23:32:16 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int result = 1;
+        
+        for (int i : box) {
+            result *= (i / n);
+        }
+        
+        return result;
+    }
+}
