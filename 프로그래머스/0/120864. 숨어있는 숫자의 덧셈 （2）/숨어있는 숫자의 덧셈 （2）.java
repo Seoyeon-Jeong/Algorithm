ecc550bee338455eaa091f52f644169a862b0390
정서연 <116091047+Seoyeon-Jeong@users.noreply.github.com>
2025-06-02 22:25:10 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.28 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        for (String s: my_string.split("\\D")) {
+            if (!s.equals("")) {
+                answer += Integer.parseInt(s);
+            }
+        }
+        return answer;
+    }
+}
